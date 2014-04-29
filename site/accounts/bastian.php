5a718b8365ab4a49082171d38fdb0901297b9a75
sashtown <git@sashtown.de>
2014-04-29 11:32:49 +0200

    excluded user accounts

--- site/accounts/bastian.php
+++
@@ -1,7 +0,0 @@
-<?php if(!defined('KIRBY')) exit ?>
-
-username: bastian
-email: mail@bastianallgeier.com
-password: >
-  $2a$10$QSv9FmFg.TaBpepggJhfG.avUhnmROsysD/gmk0kt8X.UhZ/SvZla
-token: bacb4d69110d3b34b243ad82a0f84b0c70adf598
--- site/accounts/sascha.php
+++
@@ -1,6 +0,0 @@
-<?php if(!defined('KIRBY')) exit ?>
-
-username: sascha
-email: mail@sashtown.de
-password: >
-  $2a$10$CfLkuK/BmEVMUsCXy.Y24eidj5URQcLJFmUzmUZgfRm695XT5LvOm

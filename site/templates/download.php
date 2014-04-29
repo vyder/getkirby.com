61167678bd63d70070f3cffc2dce052099717ea1
sashtown <git@sashtown.de>
2014-04-29 15:46:17 +0200

    fixed download.php

--- site/templates/download.php
+++ site/templates/download.php
@@ -1,30 +1,30 @@
 <?php snippet('header') ?>
 
 <section class="main columns download-single">
-  <h1 class="alpha"><?php echo $page->title ?></h1>
+  <h1 class="alpha"><?php echo $page->title() ?></h1>
 
   <article class="column four">
-    <p class="gamma subtitle"><?php echo $page->subtitle ?></p>
+    <p class="gamma subtitle"><?php echo $page->subtitle() ?></p>
     <div class="text">
-      <?php echo kirbytext($page->install) ?>
-      <p>Check out the <a href="<?php echo $page->docs ?>">full documentation</a> to learn more!
+      <?php echo kirbytext($page->install()) ?>
+      <p>Check out the <a href="<?php echo $page->docs() ?>">full documentation</a> to learn more!
     </div>
   </article>
 
   <aside class="column two last download-meta">
     <h2 class="gamma">Learn more!</h2>
     <ul>
-      <li><a href="<?php echo $page->link ?>">Download</a></li>
-      <li><a href="<?php echo $page->docs ?>">Documentation</a></li>
-      <?php if($page->tutorial != ''): ?>
-      <li><a href="<?php echo $page->tutorial ?>">Tutorial</a></li>
+      <li><a href="<?php echo $page->link() ?>">Download</a></li>
+      <li><a href="<?php echo $page->docs() ?>">Documentation</a></li>
+      <?php if($page->tutorial() != ''): ?>
+      <li><a href="<?php echo $page->tutorial() ?>">Tutorial</a></li>
       <?php endif ?>
     </ul>
     <div class="text">
-      <?php echo kirbytext($page->description) ?>
+      <?php echo kirbytext($page->description()) ?>
     </div>
-    <?php if($page->author != ''): ?>
-    <small>Made by <a href="<?php echo $page->author_url ?>"><?php echo $page->author ?></a><?php if($page->twitter != ''): ?> | <a href="http://twitter.com/<?php echo $page->twitter ?>"><?php echo $page->twitter ?></a><?php endif ?></small>
+    <?php if($page->author() != ''): ?>
+    <small>Made by <a href="<?php echo $page->author_url() ?>"><?php echo $page->author() ?></a><?php if($page->twitter() != ''): ?> | <a href="http://twitter.com/<?php echo $page->twitter() ?>"><?php echo $page->twitter() ?></a><?php endif ?></small>
     <?php endif ?>
   </aside>
 

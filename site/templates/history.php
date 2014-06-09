cb62d2db60ede1294f62aec445e7131e2e448a56
sashtown <git@sashtown.de>
2014-06-09 17:26:25 +0200

    1st draft of a history page + some fixes

---
+++ site/templates/history.php
@@ -0,0 +1,22 @@
+<?php snippet('header') ?>
+
+<main class="main history" role="main">
+
+  <h1 class="alpha"><?php echo kirbytext($page->title(), false) ?></h1>
+  <div class="text"><?php echo kirbytext($page->text()) ?></div>
+
+  <h2 class="beta">Milestones</h2>
+  <div class="text"><?php echo kirbytext($page->milestones()) ?></div>
+  <ul class="milestones">
+    <?php foreach($milestones = $page->children()->visible() as $milestone): ?>
+      <li>
+        <time><?php echo $milestone->date('M Y') ?></time>
+        <h3 class="gamma"><?php echo html($milestone->title()) ?></h3>
+        <p><?php echo kirbytext($milestone->description(), false) ?></p>
+      </li>
+    <?php endforeach ?>
+  </ul>
+
+</main>
+
+<?php snippet('footer') ?>

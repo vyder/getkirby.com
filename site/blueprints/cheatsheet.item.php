11e99736f2d6bbebda9f3bc1ea76fdf958b5379f
Bastian Allgeier <mail@bastianallgeier.com>
2014-08-13 11:47:55 +0200

    cheat sheet content

---
+++ site/blueprints/cheatsheet.item.php
@@ -0,0 +1,14 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+# Cheatsheet Item blueprint
+
+title: Cheat sheet item
+pages: cheatsheet.item
+files: true
+fields:
+  title:
+    label: Title
+    type:  text
+  text:
+    label: Text
+    type: textarea
---
+++ site/blueprints/cheatsheet.php
@@ -0,0 +1,14 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+# Cheatsheet blueprint
+
+title: Cheat sheet
+pages: cheatsheet.item
+files: true
+fields:
+  title:
+    label: Title
+    type:  text
+  text:
+    label: Text
+    type: textarea

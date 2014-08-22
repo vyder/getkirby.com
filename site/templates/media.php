e9b26ee20a2433f7933a6e6fd0a309de15c796f0
sashtown <git@sashtown.de>
2014-08-22 17:45:51 +0200

    Fix #1

--- site/templates/media.php
+++ site/templates/media.php
@@ -5,14 +5,15 @@
 
   <article class="col-4-6">
     <p class="gamma subtitle"><?php echo $page->subtitle() ?></p>
-    <?php $image = $page->images()->nth(1) ?>
+    <?php $image = $page->images()->nth(2) ?>
     <figure><img src="<?php echo $image->url() ?>" alt="<?php echo $page->title() ?>" /></figure>
   </article>
 
   <aside class="col-2-6 last download-meta">
     <h2 class="gamma">Grab it!</h2>
+    <?php $zip = $page->files()->filterBy('extension', '*=', 'zip')->first() ?>
     <ul>
-      <li><a href="<?php echo $page->link() ?>">Download [<?php echo $image->niceSize() ?>]</a></li>
+      <li><a href="<?php echo $zip->url() ?>">Download .zip [<?php echo $zip->niceSize() ?>]</a></li>
     </ul>
   </aside>
 

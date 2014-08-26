b06116f690e4182c7e1567483635fe9a714b6f42
sashtown <git@sashtown.de>
2014-08-26 10:57:29 +0200

    styled the cheatsheet

--- site/templates/cheatsheet.php
+++ site/templates/cheatsheet.php
@@ -9,9 +9,12 @@
     <h2 class="beta"><?php echo html($child->title()) ?></h2>
     <?php echo kirbytext($child->text()) ?>
 
+
     <?php foreach($child->children() as $doc): ?>
-    <h3 class="gamma"><?php echo html($doc->title()) ?></h3>
-    <?php echo kirbytext($doc->excerpt()) ?>
+    <div>
+      <h3 class="gamma"><?php echo html($doc->title()) ?></h3>
+      <?php echo kirbytext($doc->excerpt()) ?>
+    </div>
     <?php endforeach ?>
 
   </section>

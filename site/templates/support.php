d9402e5758a704dae40ae4665f6b829d50c83b8a
Bastian Allgeier <mail@bastianallgeier.com>
2014-08-29 12:15:14 +0200

    faqs

--- site/templates/support.php
+++ site/templates/support.php
@@ -28,11 +28,11 @@
 
     <header>
       <img src="<?php echo $page->find('answers')->image()->url() ?>" alt="FAQ">
-      <h2 class="alpha">Answers</h2>
+      <h2 class="alpha">FAQ</h2>
     </header>
 
     <div class="columns">
-      <?php foreach($page->find('answers')->children() as $answer): ?>
+      <?php foreach($page->find('answers')->children()->visible() as $answer): ?>
       <article class="answer" id="<?php echo $answer->uid() ?>">
         <h2 class="zeta"><a href="#<?php echo $answer->uid() ?>"><?php echo html($answer->title()) ?></a></h2>
         <div class="inner text">

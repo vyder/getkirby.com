78d1e6148ab098974e40748700a6ee9c77c2594a
Bastian Allgeier <mail@bastianallgeier.com>
2014-08-31 23:51:38 +0200

    fixed autocomplete

--- site/config/config.php
+++ site/config/config.php
@@ -2,17 +2,28 @@
 
 c::set('markdown.extra', true);
 
+c::set('cache.driver', 'file');
+
 c::set('routes', array(
   array(
     'pattern' => 'docs.json',
     'action'  => function() {
-      $data = page('docs')->index()->visible()->sortBy('title', 'asc')->toArray(function($item) {
-        return array(
-          'title' => $item->title()->toString(),
-          'uri'   => $item->uri()
-        );
-      });
-      return response::json(array_values($data));
+
+      $cache = new Cache\Driver\File(c::get('root.site') . DS . 'cache');
+      $data  = $cache->get('docs');
+
+      if(empty($data)) {
+        $data = page('docs')->index()->visible()->sortBy('title', 'asc')->toArray(function($item) {
+          return array(
+            'title' => $item->title()->toString(),
+            'uri'   => $item->uri()
+          );
+        });
+        $data = array_values($data);
+        $cache->set('docs', $data);
+      }
+
+      return response::json($data);
     }
   )
 ));

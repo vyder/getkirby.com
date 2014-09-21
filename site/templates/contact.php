06b32fdd607746e39d332cbe5fee57cf27274e62
Bastian Allgeier <mail@bastianallgeier.com>
2014-09-21 22:31:19 +0200

    lots of new content for the cheatsheet

--- site/templates/contact.php
+++ site/templates/contact.php
@@ -1,20 +1,17 @@
 <?php snippet('header') ?>
 
 <main class="main grid" role="main">
-  <h1 class="vh">Contact</h1>
 
-  <section class="col-3-6">
-    <h2 class="alpha margin-bottom">Contact me</h2>
-    <div class="text">
-      <?php echo kirbytext($page->text()) ?>
-    </div>
+  <h1 class="alpha"><?php echo html($page->title()) ?></h1>
+
+  <section class="col-3-6 text">
+    <h2 class="beta">Responsibility</h2>
+    <?php echo kirbytext($page->contact()) ?>
   </section>
 
-  <section class="col-3-6 last">
-    <h2 class="beta margin-bottom">Follow Kirby</h2>
-    <div class="text">
-      <?php echo kirbytext($page->web()) ?>
-    </div>
+  <section class="col-3-6 last text">
+    <h2 class="beta">Disclaimer</h2>
+    <?php echo kirbytext($page->disclaimer()) ?>
   </section>
 
 </main>

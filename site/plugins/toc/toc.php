ba767278a2cdb7c88e7337db6cb6dc0ea3eee054
sashtown <git@sashtown.de>
2014-10-02 16:00:30 +0200

    add toc class (docs)

--- site/plugins/toc/toc.php
+++ site/plugins/toc/toc.php
@@ -7,6 +7,7 @@ kirbytext::$post[] = function($kirbytext, $value) {
     preg_match_all('!<h2>(.*)</h2>!', $value, $matches);
 
     $ul = brick('ul');
+    $ul->addClass('toc');
 
     foreach($matches[1] as $match) {
       $li = brick('li', '<a href="#' . str::slug($match) . '">' . $match . '</a>');

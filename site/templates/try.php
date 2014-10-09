68f289ea447ed291c652ef7e6477db5a305a8a4e
sashtown <git@sashtown.de>
2014-10-09 12:48:52 +0200

    modify try page

--- site/templates/try.php
+++ site/templates/try.php
@@ -3,18 +3,13 @@
 <main class="main" role="main">
 
   <h1 class="alpha"><?php echo html($page->headline()) ?></h1>
-  <p class="beta margin-bottom"><?php echo html($page->subheadline()) ?></p>
 
-  <div class="grid">
+  <div class="grid text">
     <div class="col-3-6">
-
-      <div class="text">
-        <?php echo kirbytext($page->text()) ?>
-      </div>
-
+      <?php echo kirbytext($page->text()) ?>
     </div>
     <div class="col-3-6 last">
-      <figure class="screenshot framed"><a href="http://github.com/getkirby" title="Watch Kirby's organization on GitHub"><img src="<?php echo $page->image()->url() ?>" alt="Screenshot GitHub repo Kirby" /></a></figure>
+      <?php echo kirbytext($page->getstarted()) ?>
     </div>
   </div>
 

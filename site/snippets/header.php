48920f89508e9b1c5c7e5c4c0e44431d9fdbd954
Bastian Allgeier <mail@bastianallgeier.com>
2014-10-13 10:53:13 +0200

    Updated Google Analytics Code

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -29,25 +29,21 @@
   <meta name="apple-mobile-web-app-title" content="<?php echo html($site->title()) ?>">
   <link rel="alternate" type="application/rss+xml" href="<?php echo url('feed') ?>" title="<?php echo html($site->title()) ?> Blog Feed" />
 
+</head>
+<body class="<?php e(c::get('stage'), 'stage ') ?><?php echo str_replace('.', '-', $page->template()) ?>">
+
   <?php if(server::get('SERVER_NAME') == 'getkirby.com'): ?>
-  <script type="text/javascript">
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-431401-11']);
-    _gaq.push(['_setDomainName', 'getkirby.com']);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
+    ga('create', 'UA-431401-11', 'auto');
+    ga('send', 'pageview');
 
   </script>
   <?php endif ?>
-
-</head>
-<body class="<?php e(c::get('stage'), 'stage ') ?><?php echo str_replace('.', '-', $page->template()) ?>">
 
   <!--[if lte IE 9]>
   <div class="browserupdate">

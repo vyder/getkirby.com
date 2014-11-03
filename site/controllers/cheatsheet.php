da7b20573e8b153ccbd77010e0e26e33fcb51114
Bastian Allgeier <mail@bastianallgeier.com>
2014-11-03 17:12:13 +0100

    Better cheatsheet loading with snippet cache

---
+++ site/controllers/cheatsheet.php
@@ -0,0 +1,16 @@
+<?php
+
+return function($site, $pages, $page) {
+
+  $cache   = new Cache\Driver\File(kirby()->roots()->cache());
+  $cacheId = $page->cacheId() . '.main';
+  $content = $cache->get($cacheId);
+
+  if(empty($content)) {    
+    $content = snippet('cheatsheet', array('page' => $page), true);
+    $cache->set($cacheId, $content);
+  }
+
+  return compact('content');
+
+};

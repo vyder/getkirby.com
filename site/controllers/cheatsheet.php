8f247bb3146d9ac733499543b5154d8bd9c23770
Bastian Allgeier <mail@bastianallgeier.com>
2014-11-05 21:16:56 +0100

    Fixed cheatsheet controller

--- site/controllers/cheatsheet.php
+++ site/controllers/cheatsheet.php
@@ -5,7 +5,6 @@ return function($site, $pages, $page) {
   $cache   = new Cache\Driver\File(kirby()->roots()->cache());
   $cacheId = $page->cacheId() . '.main';
   $content = $cache->get($cacheId);
-  $content = null;
 
   if(empty($content)) {    
     $content = snippet('cheatsheet', array('page' => $page), true);

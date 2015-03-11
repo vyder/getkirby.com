ec54cc0016645f65aa705d875cfdf13e4453663d
Vidur Murali <vidur.murali@gmail.com>
2015-03-11 19:16:12 +0530

    Fix for #31: Fix autocomplete links by making them absolute uri's on navigation

--- assets/js/templates/docs.index.js
+++ assets/js/templates/docs.index.js
@@ -1,5 +1,5 @@
 // Add section anchors to every <h2> + <h3> within docs
 
 $('#q').autocomplete().on('autocomplete:add', function(e, item) {
-  window.location.href = item.uri;
+    window.location.href = "/" + item.uri;
 });
